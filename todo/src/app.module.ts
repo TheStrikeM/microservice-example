e8734457a8c8f7465cd015262dc4b2bb8c829a6d
TheStrikeM <thestrikem@gmail.com>
2021-04-07 09:39:19 +0500

    Initial commit, create todo app module, create file acrhitecture in todo and more

---
+++ todo/src/app.module.ts
@@ -0,0 +1,20 @@
+import {Logger, Module, OnApplicationBootstrap, OnApplicationShutdown} from "@nestjs/common";
+
+
+const mainLogger = new Logger('Main')
+
+@Module({
+    imports: [],
+    providers: [],
+    controllers: [],
+    exports: []
+})
+export default class AppModule implements OnApplicationBootstrap, OnApplicationShutdown {
+    onApplicationBootstrap(): void {
+        mainLogger.log(`Server has success started!`)
+    }
+
+    onApplicationShutdown(signal?: string): void {
+        mainLogger.log(`Server has success stopped!`)
+    }
+}
